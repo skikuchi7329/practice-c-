89e043ac4aacc96131c4f0a26bed774177968e76
Shohei Kikuchi <killchance2256@gmail.com>
2021-06-17 22:24:37 +0900

    06b.c

---
+++ playground/06b.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(void)
+{
+  int i, j;
+  int pl, mi, mu, di;
+  i = 50.;
+  j = 20.;
+  pl = i + j;
+  mi = i - j;
+  mu = i * j;
+  di = i / j;
+  printf("i+j=%f¥n", pl);
+  printf("i-j=%f¥n", mi);
+  printf("i*j=%f¥n", mu);
+  printf("i/j=%f¥n", di);
+  return 0;
+}
