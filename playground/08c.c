b0de2379d318229610565df0d221989201ebad07
Shohei Kikuchi <killchance2256@gmail.com>
2021-06-21 19:07:13 +0900

    08c.c

---
+++ playground/08c.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main(void)
+{
+  int ondo;
+  ondo = 0;
+  while (1) {
+    printf("温度を入力： ");
+    scanf_s("%d", &ondo);
+    if (ondo == 999) break;
+    if (ondo >= 26) {
+      printf("HOT¥n");
+    } else if (ondo <= 15) {
+      printf("COLD¥n"); 
+    } else {
+      printf("WELL¥n");
+    }
+  }
+  return 0;
+}
