e91d0f967e9ff3edfadec8ce43a7af0f8c78f05a
Shohei Kikuchi <killchance2256@gmail.com>
2021-06-29 16:15:34 +0900

    fix

--- playground/10d.c
+++ playground/10d.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 int main(void) 
 {
-  int i;
+  int s = 0, k = 0;
   double kAve, sAve, s[i];
   int k[5] = {92, 56, 89, 76, 81};
   int s[5] = {82, 45, 79, 87, 91};
   for (i = 0; i < 5; i++) {
-    kAve += k[i] / 5.;
-    sAve += s[i] / 5.;
+    k += k[i];
+    s += s[i];
   }
+  kAve = k / 5.0;
+  sAve = s / 5.0;
   printf ("国語の平均点は%d¥n点です。", kAve);
   printf ("数学の平均点は%d¥n点です。", sAve);
   for (i < 0; i < 5; i++) {
